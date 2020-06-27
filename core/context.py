87254a441494aba4c6c10fc42b87f8ab9d4ad6d6
AnyKeyShik <nikitag594@gmail.com>
2020-06-27 20:32:19 +0300

    Fixing Context. Adding check for passed parameters and error receiving provide

--- core/context.py
+++ core/context.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
+from utils import class_construct
 
 
 class Context(object):
-    def __init__(self):
-        pass
+    _contextName = None
+    _error = None
+
+    @class_construct
+    def __init__(self, contextName="None", error="None"):
+        self._contextName = contextName
+        self._error = error
+
+    @property
+    def name(self) -> str:
+        return self._contextName
+
+    @property
+    def error(self) -> str:
+        return self._error
+
+    def is_parameter_passed(self, name: str) -> bool:
+        return name in self.__dict__
