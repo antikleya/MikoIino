ae11c4784fd0b21064a629be7f4eb0dcc7a459cf
antikleya <anitkleya@yandex.ru>
2020-07-12 18:36:35 +0300

    made riot api functions and service into different files

---
+++ riot_api/clash_schedule_service.py
@@ -0,0 +1,28 @@
+import asyncio
+from riot_api import get_tournament_schedule, get_clash_time
+path_to_api = 'key'
+port = 1488
+key = open(path_to_api).read()
+
+
+async def handle_client(reader, writer):
+    request = (await reader.read(255)).decode('utf8')
+    split_request = request.split()
+    val_req = ' '.join(split_request[0:3])
+    if (len(val_req) < 3) or (val_req not in valid_requests.keys()):
+        response = 'Not a valid request ' + val_req
+    else:
+        response = valid_requests[val_req](split_request, key)
+        if not response.valid:
+            response = 'Error: ' + response.text
+        else:
+            response = response.text
+    writer.write(response.encode('utf8'))
+    await writer.drain()
+    writer.close()
+
+
+valid_requests = {'get tournament schedule': get_tournament_schedule, 'get clash time': get_clash_time}
+loop = asyncio.get_event_loop()
+loop.create_task(asyncio.start_server(handle_client, 'localhost', 1488))
+loop.run_forever()
--- riot_api/riot_api.py
+++ riot_api/riot_api.py
@@ -1,11 +1,6 @@
 import requests
-import asyncio
-import re
 
-path_to_api = 'key'
-port = 1488
 valid_regions = {'EUW': 'euw1', 'RU': 'ru'}
-key = open(path_to_api).read()
 
 
 class ReturnedValue:
@@ -18,17 +13,17 @@ class ReturnedValue:
 
 
 def get_time_shift(tier: str):
-    if tier == '1':
-        return 0
-    if tier == '2':
-        return 145 * 60
-    if tier == '3':
-        return 180 * 60
     if tier == '4':
+        return 0
+    if tier == '3':
+        return 145 * 60
+    if tier == '2':
+        return 180 * 60
+    if tier == '1':
         return 210 * 60
 
 
-def get_summoner_id(region: str, summoner_name: str):
+def get_summoner_id(region: str, summoner_name: str, key: str):
     url = 'https://' + region + '.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summoner_name + '?api_key=' + key
     response = requests.get(url)
     if response.status_code != 200:
@@ -38,7 +33,7 @@ def get_summoner_id(region: str, summoner_name: str):
     return ReturnedValue(True, str(summoner_id))
 
 
-def get_team_id(region: str, summoner_id: str):
+def get_team_id(region: str, summoner_id: str, key: str):
     url = 'https://' + region + '.api.riotgames.com/lol/clash/v1/players/by-summoner/' + summoner_id + '?api_key=' + key
     response = requests.get(url)
     if response.status_code != 200:
@@ -48,7 +43,7 @@ def get_team_id(region: str, summoner_id: str):
     return ReturnedValue(True, str(team_id))
 
 
-def get_tier(region: str, team_id: str):
+def get_tier(region: str, team_id: str, key: str):
     url = 'https://' + region + '.api.riotgames.com/lol/clash/v1/teams/' + team_id + '?api_key=' + key
     response = requests.get(url)
     if response.status_code != 200:
@@ -58,7 +53,7 @@ def get_tier(region: str, team_id: str):
     return ReturnedValue(True, str(team_tier))
 
 
-def get_tournament_schedule(request: list):
+def get_tournament_schedule(request: list, key: str):
     if len(request) < 4:
         return ReturnedValue(False, 'Not a valid request')
 
@@ -79,21 +74,21 @@ def get_tournament_schedule(request: list):
     return ReturnedValue(True, schedule)
 
 
-def get_clash_time(request: list):
-    uncorrected_schedule = get_tournament_schedule(request)
+def get_clash_time(request: list, key: str):
+    uncorrected_schedule = get_tournament_schedule(request, key)
     if not uncorrected_schedule.valid:
         return uncorrected_schedule
 
     uncorrected_schedule = list(map(int, uncorrected_schedule.text.split()))
     region = valid_regions[request[3].upper()]
     summoner_name = request[4]
-    summoner_id = get_summoner_id(region, summoner_name)
+    summoner_id = get_summoner_id(region, summoner_name, key)
     if not summoner_id.valid:
         return summoner_id
-    team_id = get_team_id(region, summoner_id.text)
+    team_id = get_team_id(region, summoner_id.text, key)
     if not team_id.valid:
         return team_id
-    tier = get_tier(region, team_id.text)
+    tier = get_tier(region, team_id.text, key)
     if not tier.valid:
         return tier
     shift = get_time_shift(tier.text)
@@ -101,26 +96,3 @@ def get_clash_time(request: list):
         uncorrected_schedule[i] = str(uncorrected_schedule[i] + shift)
     corrected_schedule = ' '.join(uncorrected_schedule)
     return ReturnedValue(True, corrected_schedule)
-
-
-async def handle_client(reader, writer):
-    request = (await reader.read(255)).decode('utf8')
-    split_request = request.split()
-    val_req = ' '.join(split_request[0:3])
-    if (len(val_req) < 3) or (val_req not in valid_requests.keys()):
-        response = 'Not a valid request ' + val_req
-    else:
-        response = valid_requests[val_req](split_request)
-        if not response.valid:
-            response = 'Error: ' + response.text
-        else:
-            response = response.text
-    writer.write(response.encode('utf8'))
-    await writer.drain()
-    writer.close()
-
-
-valid_requests = {'get tournament schedule': get_tournament_schedule, 'get clash time': get_clash_time}
-loop = asyncio.get_event_loop()
-loop.create_task(asyncio.start_server(handle_client, 'localhost', 1488))
-loop.run_forever()
